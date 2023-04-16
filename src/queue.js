91176b1b6c0f3bbaae8101f5bab19a60e64533d3
Eugene Zanevskij <zanevskijzenia@gmail.com>
2023-04-16 20:12:59 +0300

    feat: queue.js

--- src/queue.js
+++ src/queue.js
@@ -13,23 +13,60 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.next = null;
+  }
+}
+
 class Queue {
+  constructor() {
+    this.root = null;
+    this.tail = null;
+    this.length = 0;
+  }
 
   getUnderlyingList() {
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  enqueue(value) {
+    const node = new Node(value);
+
+    if (this.root) {
+      this.tail.next = node;
+      this.tail = node;
+
+    } else {
+      this.root = node;
+      this.tail = node;
+    }
+
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const current = this.root;
+    this.root = this.root.next;
+    this.length--;
+
+    return current.value;
+  }
+
+  getUnderlyingList() {
+    return this.root;
   }
 }
+
+const queue = new Queue();
+
+queue.enqueue(1); // adds the element to the queue
+queue.enqueue(3); // adds the element to the queue
+queue.enqueue(3); // adds the element to the queue
+queue.dequeue(); // returns the top element from queue and deletes it, returns 1
+queue.getUnderlyingList() // returns { value: 3, next: null }
 
 module.exports = {
   Queue
